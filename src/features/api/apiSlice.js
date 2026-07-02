a4995d0013f49282ee891de7b60a5f7af78f5c8b
Yusra <laraibyusra519@gmail.com>
2026-07-02 20:08:50 +0500

    Update apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,7 +5,7 @@ export const apiSlice=createApi({
     reducerPath:"api",
     baseQuery:fetchBaseQuery({baseUrl:"https://6a3bc652e4a07f202e15d35b.mockapi.io/"}),
     endpoints:(builder)=>({
-        getProduct:builder.query({ query : ()=> "Products"})
+        getProduct:builder.query({ query : ()=> "Products",keepUnusedDataFor: 300,})
     })
 })
 
