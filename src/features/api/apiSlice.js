9d475012fa368624d517f868bcb9fe3fd6030fc8
Yusra <laraibyusra519@gmail.com>
2026-07-03 19:27:09 +0500

    Update apiSlice.js

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,8 +4,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice=createApi({
     reducerPath:"api",
     baseQuery:fetchBaseQuery({baseUrl:"https://6a3bc652e4a07f202e15d35b.mockapi.io/"}),
+    tagTypes:["Products"],
     endpoints:(builder)=>({
-        getProduct:builder.query({ query : ()=> "Products",keepUnusedDataFor: 300,})
+        getProduct:builder.query({ 
+            query : ()=> "Products",
+            keepUnusedDataFor: 300,
+            providesTags: ["Products"]
+        })
     })
 })
 
